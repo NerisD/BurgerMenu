5a4eab26b4435f9ca1b06f91aa19163e5b7ba9a6
Dimitri SMITH <dimitri.smith_external@shadow.tech>
2022-05-21 14:13:44 +0400

    First Commit

---
+++ BurgerMenu_App/BurgerMenu_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BurgerMenu_App
+//
+//  Created by Dimitri SMITH on 21/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
