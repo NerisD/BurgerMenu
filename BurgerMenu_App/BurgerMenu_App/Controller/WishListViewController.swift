63d44a0584bd0a9753d949fcd1c9010da4a99a05
Dimitri SMITH <dimitri.smith_external@shadow.tech>
2022-05-23 20:21:09 +0400

    #7 - WishList is available and the user can see it. wip : - display the total price to pay - improuve data management when you want to update your wishlist

--- BurgerMenu_App/BurgerMenu_App/Controller/WishListViewController.swift
+++ BurgerMenu_App/BurgerMenu_App/Controller/WishListViewController.swift
@@ -7,15 +7,164 @@
 
 import Foundation
 import UIKit
+import CoreData
 
 
 class WishListViewController: UIViewController {
     
     
+    @IBOutlet weak var wlBurgerListTableView: UITableView!
+    @IBOutlet weak var applePayLogo: UIImageView!
+    @IBOutlet weak var wlTotalToPay: UILabel!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    @IBOutlet weak var messageLabel: UILabel!
     
     
     
     
     
+    private let persistentContainer = NSPersistentContainer(name: "BurgerMenu_App")
     
+    fileprivate lazy var fetchedResultsController: NSFetchedResultsController<BurgerMenuDB> = {
+        
+        // Create Fetch Request
+        let fetchRequest: NSFetchRequest<BurgerMenuDB> = BurgerMenuDB.fetchRequest()
+        
+        // Configure Fetch Request
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "createdAt", ascending: true)]
+        
+        // Create Fetched Results Controller
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.persistentContainer.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        // Configure Fetched Results Controller
+        fetchedResultsController.delegate = self
+
+        return fetchedResultsController
+    }()
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        wlBurgerListTableView.delegate = self
+        wlBurgerListTableView.dataSource = self
+        
+        persistentContainer.loadPersistentStores { (persistentStoreDescription, error) in
+            if let error = error {
+                print("Unable to Load Persistent Store")
+                print("\(error), \(error.localizedDescription)")
+
+            } else {
+                self.setupView()
+
+                do {
+                    try self.fetchedResultsController.performFetch()
+                } catch {
+                    let fetchError = error as NSError
+                    print("Unable to Perform Fetch Request")
+                    print("\(fetchError), \(fetchError.localizedDescription)")
+                }
+
+                self.updateView()
+            }
+        }
+    }
+    
+    private func setupView() {
+        setupMessageLabel()
+
+        updateView()
+    }
+
+    private func updateView() {
+        var hasBurgers = false
+
+        if let burgers = fetchedResultsController.fetchedObjects {
+            hasBurgers = burgers.count > 0
+        }
+
+        wlBurgerListTableView.isHidden = !hasBurgers
+        messageLabel.isHidden = hasBurgers
+
+        activityIndicatorView.stopAnimating()
+    }
+    
+    private func setupMessageLabel() {
+        messageLabel.text = "You don't have any burger yet."
+    }
+    
+}
+extension WishListViewController: NSFetchedResultsControllerDelegate {
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        wlBurgerListTableView.beginUpdates()
+    }
+
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        wlBurgerListTableView.endUpdates()
+
+        updateView()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch (type) {
+        case .insert:
+            if let indexPath = newIndexPath {
+                wlBurgerListTableView.insertRows(at: [indexPath], with: .fade)
+            }
+            break;
+        case .delete:
+            if let indexPath = indexPath {
+                wlBurgerListTableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            break;
+        case .update:
+            if let indexPath = indexPath, let cell = wlBurgerListTableView.cellForRow(at: indexPath) as? WishListCustomViewCell {
+                // *TODO Update Cell
+            }
+            break;
+        case .move:
+            if let indexPath = indexPath {
+                wlBurgerListTableView.deleteRows(at: [indexPath], with: .fade)
+            }
+
+            if let newIndexPath = newIndexPath {
+                wlBurgerListTableView.insertRows(at: [newIndexPath], with: .fade)
+            }
+            break;
+        @unknown default:
+            print(Error.self)
+        }
+    }
+
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+
+    }
+
+}
+
+extension WishListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        guard let burgers = fetchedResultsController.fetchedObjects else { return 0 }
+        return burgers.count
+
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "wishListCell", for: indexPath) as? WishListCustomViewCell else {
+            fatalError("Unexpected Index Path")
+        }
+        
+        // Fetch Quote
+        let burgerMenuDB = fetchedResultsController.object(at: indexPath)
+        
+        cell.wlBurgerImage.loadImageUsingCache(with: burgerMenuDB.thumbnail!)
+        cell.wlBurgerTitleLabel.text = burgerMenuDB.title
+        cell.wlBurgerDescriptionLabel.text = burgerMenuDB.details
+        cell.wlUnitPriceLabel.text = burgerMenuDB.price
+        cell.wlTotalUnitLabel.text = String(burgerMenuDB.nombre)
+        
+        return cell
+    }
 }
--- BurgerMenu_App/BurgerMenu_App/View/WishListCustomViewCell.swift
+++ BurgerMenu_App/BurgerMenu_App/View/WishListCustomViewCell.swift
@@ -11,6 +11,14 @@ import UIKit
 
 class WishListCustomViewCell: UITableViewCell {
     
+    @IBOutlet weak var wlBurgerImage: UIImageView!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var wlBurgerTitleLabel: UILabel!
+    @IBOutlet weak var wlBurgerDescriptionLabel: UILabel!
+    @IBOutlet weak var wlUnitPriceLabel: UILabel!
+    @IBOutlet weak var wlTotalUnitLabel: UILabel!
+    
     
     
     
