41961bf6a1e9b77dacf2aaa5c4db3ec3647c6f96
Dimitri SMITH <dimitri.smith_external@shadow.tech>
2022-05-23 13:20:07 +0400

    Ref : #7 - When user make a long press on a burger, the object he choose is save in CoreData

--- BurgerMenu_App/BurgerMenu_App/Controller/ViewController.swift
+++ BurgerMenu_App/BurgerMenu_App/Controller/ViewController.swift
@@ -6,30 +6,44 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var burgerListTableView: UITableView!
     
-    
     var urlBurgerComponent = URLComponents()
     var burger: [Burger] = []
     
+    var appDelegate: AppDelegate!
+    var context: NSManagedObjectContext!
+    var burgerToCoreData : BurgerMenuDB!
+    var burgers:[NSManagedObject] = []
     
-    
-    
-    
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(saveToCoreData))
+        burgerListTableView.addGestureRecognizer(longPress)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         burgerListTableView.delegate = self
         burgerListTableView.dataSource = self
         getApiBugerData()
+        
+        DispatchQueue.main.async {
+            self.appDelegate = UIApplication.shared.delegate as? AppDelegate
+            self.context = self.appDelegate.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<NSManagedObject> (entityName: "BurgerMenuDB")
+            
+            do {
+                self.burgers = try self.context.fetch(fetchRequest)
+                
+            } catch {
+                print("Error for Entity initialisation \(error)")
+            }
+            
+        }
     }
     
     
@@ -40,7 +54,6 @@ class ViewController: UIViewController {
         
         return URL(string: urlBurgerComponent.url?.absoluteString ?? "https://uad.io/bigburger/")!
     }
-    
     func getApiBugerData() {
         let urlBurgerAPI = composeURLForBurgerList()
         var request = URLRequest (url: urlBurgerAPI)
@@ -67,6 +80,50 @@ class ViewController: UIViewController {
             }
         }.resume()
     }
+    
+    @objc func saveToCoreData(sender: UILongPressGestureRecognizer) {
+
+                if sender.state == UIGestureRecognizer.State.began {
+                    let touchPoint = sender.location(in: burgerListTableView)
+                    if let indexPath = burgerListTableView.indexPathForRow(at: touchPoint) {
+                        print("Long press Pressed:\(indexPath)")
+                        
+                        let request: NSFetchRequest<BurgerMenuDB> = BurgerMenuDB.fetchRequest()
+                        
+                        request.predicate = NSPredicate(format: "ref LIKE %@", burger[indexPath.row].ref!)
+                        
+                        do {
+                            let result = try context.fetch(request)
+                            if result.count == 0 {
+                                burgerToCoreData = BurgerMenuDB(context: context)
+                            } else {
+                                burgerToCoreData = result.first
+                            }
+                            
+                            context.performAndWait {
+                                burgerToCoreData.nombre += 1
+                                burgerToCoreData.ref = burger[indexPath.row].ref
+                                burgerToCoreData.thumbnail = burger[indexPath.row].thumbnail
+                                burgerToCoreData.title = burger[indexPath.row].title
+                                burgerToCoreData.price = "\(burger[indexPath.row].price!.formatnumber())"
+                                burgerToCoreData.details = burger[indexPath.row].description
+                                
+                                do {
+                                    print("Save to CoreData")
+                                    try context.save()
+                                }catch {
+                                    print("Saving to CoreData Error : \(error)")
+                                }
+                            }
+                            
+                        }catch {
+                            fatalError("Unresolved error \(error)")
+                        }
+                    }
+                }
+
+
+            }
 
 
 }
